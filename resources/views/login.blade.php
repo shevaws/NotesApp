d2ecb96727c561c34b35de1ab4d0ca8ca857e345
shevaws <xhuagf299@gmail.com>
2026-04-27 16:19:36 +0700

    fitur login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login</title>
+</head>
+<body>
+<h1>Login Page</h1>
+
+<form>
+<input type="text" placeholder="Username"><br><br>
+<input type="password" placeholder="Password"><br><br>
+<button>Login</button>
+</form>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NoteController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/login', function () {
+    return view('login');
 });
 
 Route::middleware(['auth'])->group(function () {

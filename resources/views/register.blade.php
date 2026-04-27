fc4b52527705b80b49f833f09e2447e917810322
Faza01 <fazabilwidi@gmail.com>
2026-04-27 16:42:33 +0700

    fitur register

---
+++ resources/views/register.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Register</title>
+</head>
+<body>
+<h1>Register Page</h1>
+
+<form method="POST">
+<input type="text" placeholder="Nama"><br><br>
+<input type="email" placeholder="Email"><br><br>
+<input type="password" placeholder="Password"><br><br>
+<button>Register</button>
+</form>
+
+</body>
+</html>
